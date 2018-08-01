7a35c5462625e0624f885e32ea9a907ddc75c50a
Klaus Dresbach <klaus.dresbach@t-online.de>
2018-08-01 21:04:02 +0200

    180801 Add new map-for-itinerary feature

---
+++ TaufiLearnsGit/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  TaufiLearnsGit
+//
+//  Created by Klaus Dresbach on 01.08.18.
+//  Copyright © 2018 Klaus Dresbach. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
