02e3cc48f71f43200418d6c4cddc1c16d1a5d368
Klaus Dresbach <klaus.dresbach@t-online.de>
2018-08-01 20:55:49 +0200

    180801 Nachricht, wenn die App gestartet ist. Button, der etwas auf der Konsole ausgibt, wenn er gedrückt wird

--- TaufiLearnsGit/ViewController.swift
+++ TaufiLearnsGit/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
+  
+  @IBAction func buttonClicked(_ sender: UIButton) {
+    print("Dies ist ein Git-Tutorial")
+  }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
